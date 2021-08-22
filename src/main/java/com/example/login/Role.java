f83e3ad93f921dda529b935b2ef8f90f7eef07b9
tlawl <tlawltjs92@gmail.com>
2021-08-22 21:16:43 +0900

    Initial commit

---
+++ src/main/java/com/example/login/Role.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Role {
+
+    MEMBER("ROLE_MEMBER"),
+    ADMIN("ROLE_ADMIN");
+
+    private String value;
+}
